2632db3007f4f0c20567a291803d260fef164ce0
Miguel Tavares <m.tavares51@hotmail.com>
2017-03-22 19:21:16 +0000

    Adicionei os icons na homepage e criei três ficheiros para cada icon(Barril.php,Camara.php,Carro.php)

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,1 +1,9 @@
-asdasd
+<a href="<?php echo base_url();?>application/controllers/Carro.php">
+<img src="<?php echo base_url();?>assets/imgs/carro.png" style="height: 400px; margin-top:16px; margin-left:30px">
+</a>
+<a href="<?php echo base_url();?>application/controllers/Barril.php">
+<img src="<?php echo base_url();?>assets/imgs/barril.png" style="height: 300px; margin-top:16px; margin-left:450px">
+</a>
+<a href="<?php echo base_url();?>application/controllers/Camara.php">
+<img src="<?php echo base_url();?>assets/imgs/camara.jpeg" style="height: 250px; margin-top:30px; margin-left:470px">
+</a>
