6be3898f96ab3263b79a88aa6b28c9366f430c4b
Miguel Tavares <m.tavares51@hotmail.com>
2017-04-13 23:04:51 +0100

    View para o aluger de carros incompleta

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -10,8 +10,27 @@ class Test extends CI_Controller {
 
     public function index()
     {
+
     	$this->load->view('template/header');
-		$this->load->view('homepage');
+		$this->load->view('aluguer_carro');
 		$this->load->view('template/footer');
+        
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('user','text','trim');
+        
+        if($this->form_validation->run())
+        {   
+            if($this->input->post("login") == 'registar')
+            {
+                echo 'Registar: '.$this->input->post("pass");
+            }
+            if($this->input->post("login") == 'login')
+            {
+                echo 'Login: '.$this->input->post("pass");
+            }
+        }
+
     }
 }
