9d7b396bd934c8a7a0b011d4bbd16586c43cca0d
caninooje69 <fernandoprates_96@hotmail.com>
2017-04-26 00:20:51 +0100

    view reserva-carros

--- application/views/aluguer_carro.php
+++ application/views/aluguer_carro.php
@@ -3,47 +3,74 @@
 <div class="container" style="color: white">
 	<form method="post">
 		<div class="form-group">
-			<label>Nome do requsitor:</label>
-	  		<input type="text" name="user"  style="width:300px" class="form-control" id="usr">
+			<label>Id do requsitor:</label>
+	  		<input type="number" name="id"  style="width:300px" class="form-control">
 		</div>
-		<div class="form-group">
-	  		<label>Marca do Carro:</label>
-	  		<input name="marcaCarro" type="text" style="width: 300px" class="form-control" id="marcaCarro">
-		</div>
-		<div class="form-group">
-			<label>Número carta condução</label>
-			<input name="conducao" type="number" style="width: 300px" class="form-control" id="cartaCondução">
-		</div>
-
-		<div class="form-group">
-			<label>Número cartão cidadão</label>
-			<input name="cc" type="text" style="width: 300px" class="form-control" id="cc">
-		</div>
-
-		
-
-
 
 		<div class="form-group row">
 			<div class="col col-xs-6">
 				<label>Data Inicial</label>
-				<input name="dataInicial" type="datetime-local" style="width: 300px" class="form-control" id="dataInicial" >
+				<input name="data_inicial" type="text" style="width: 300px" class="form-control">
 			</div>
 			<div class="col col-xs-6">
 				<label>Data Final</label>
-				<input name="dataFinal" type="datetime-local" style="width: 300px" class="form-control" id="dataFinal">
+				<input name="data_final" type="text" style="width: 300px" class="form-control">
 			</div>
 		</div>
 
 		<div class="form-group row">
 			<div class="col-xs-6">
 				<label>Km Inicial</label>
-				<input name="kmInicial" type="number" style="width: 300px" class="form-control" id="kmInicial">
+				<input name="km_iniciais" type="number" style="width: 300px" class="form-control" id="kmInicial">
 			</div>
 			<div class="col-xs-6">
 				<label>Km Final</label>
-				<input name="kmFinal" type="number" style="width: 300px" class="form-control" id="kmFinal">
+				<input name="km_finais" type="number" style="width: 300px" class="form-control">
 			</div>
+		</div>
+		<div class="form-group row">
+			<div class="col-xs-6">
+				<label>Combustivel Inicial</label>
+				<input name="comb_inicial" type="number" style="width: 300px" class="form-control">
+			</div>
+			<div class="col-xs-6">
+				<label>Combustivel Final</label>
+				<input name="comb_final" type="number" style="width: 300px" class="form-control">
+			</div>
+
+		</div>
+		<div class="form-group">
+			<label>Identificação:</label>
+	  		<input type="text" name="identificacao"  style="width:300px" class="form-control">
+		</div>
+		
+		<div class="form-group">
+			<label>Nome</label>
+	  		<input type="text" name="nome"  style="width:300px" class="form-control">
+		</div>
+		
+		<div class="form-group">
+			<label>Ocorrencias</label>
+	  		<input type="text" name="ocorrencias"  style="width:300px" class="form-control">
+		</div>
+	
+		<div class="form-group">
+			<label for="sel1">Id do carro:</label>
+			<select class="form-control" id="sel1" style="width:300px">
+				<option>conas</option> 
+				<option>Fusco é gay</option>
+			</select>
+	  		
+		</div>
+		
+		<div class="form-group">
+			<label>Número de carta de condução: </label>
+	  		<input type="text" name="nr_carta_cond"  style="width:300px" class="form-control">
+		</div>
+		
+		<div class="form-group">
+			<label>Contacto</label>
+	  		<input type="text" name="contacto"  style="width:300px" class="form-control">
 		</div>
 
 
