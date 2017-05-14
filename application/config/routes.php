97c7101692df3f376da08d61115467c9edb16d85
[Henrique Raposo] <[henrique.raposo95@gmail.com]>
2017-05-14 18:40:45 +0100

    alteraçoes na homepage

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'homepage';
+$route['carros'] = 'carros';
 $route[LOGIN_PAGE] = 'examples/login';
 
 $route['calendario'] = 'calendario';
